9329c6324df869af594ebefeb1b7df8bae1f5339
abhinesh <abhinesh.c@eegrab.com>
2021-03-31 08:43:45 +0530

    added retrofit instance

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "1aef8bff01b34e56b931da95d6c2330f"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
