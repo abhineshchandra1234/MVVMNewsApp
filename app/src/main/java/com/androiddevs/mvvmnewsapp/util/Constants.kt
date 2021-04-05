aad25702c61bc29c3df409d9fa7ecc02c74831a3
abhinesh <abhinesh.c@eegrab.com>
2021-04-05 13:34:30 +0530

    added webview for articles, app is working fine

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "1aef8bff01b34e56b931da95d6c2330f"
         const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
